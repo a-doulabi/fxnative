ad99ac2f1f8f1444a6b4705a17196b625e421240
Ali Doulabi <dark.knight.96162@gmail.com>
2025-08-31 17:20:22 +0330

    changed eas config again

--- app.config.js
+++ app.config.js
@@ -1,27 +1,18 @@
-export default {
-  expo: {
-    name: "Filmonix",
-    slug: "filmonix",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    scheme: "filmonix",
-    userInterfaceStyle: "automatic",
-    runtimeVersion: {
-      policy: "appVersion"
+export default ({ config }) => {
+  return {
+    ...config,
+    extra: {
+      ...config.extra,
+      eas: {
+        projectId: "7c018731-19d5-48d1-aeab-9eb3300399d3" // 👈 همونی که توی ارور بهمون داده
+      }
     },
     updates: {
-      url: "https://u.expo.dev/7c018731-19d5-48d1-aeab-9eb3300399d3", // 👈 projectId از expo.dev
+      url: "https://u.expo.dev/7c018731-19d5-48d1-aeab-9eb3300399d3",
       enabled: true,
       checkAutomatically: "ON_LOAD",
       fallbackToCacheTimeout: 0,
-      channel: "production" // 👈 کانالی که بیلد بهش وصله
-    },
-    android: {
-      package: "fxman.filmonix"
-    },
-    ios: {
-      bundleIdentifier: "fxman.filmonix"
+      channel: "production"
     }
-  }
+  };
 };
