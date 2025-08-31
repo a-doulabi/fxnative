0a2d6ec0338a6d6674258b2e8238c0fe6403ef7c
Ali Doulabi <dark.knight.96162@gmail.com>
2025-08-31 17:15:36 +0330

    change app config

--- app.config.js
+++ app.config.js
@@ -1,12 +1,27 @@
-export default ({ config }) => {
-  return {
-    ...config,
+export default {
+  expo: {
+    name: "Filmonix",
+    slug: "filmonix",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "filmonix",
+    userInterfaceStyle: "automatic",
+    runtimeVersion: {
+      policy: "appVersion"
+    },
+    updates: {
+      url: "https://u.expo.dev/7c018731-19d5-48d1-aeab-9eb3300399d3", // 👈 projectId از expo.dev
+      enabled: true,
+      checkAutomatically: "ON_LOAD",
+      fallbackToCacheTimeout: 0,
+      channel: "production" // 👈 کانالی که بیلد بهش وصله
+    },
     android: {
-      ...config.android,
-      metaData: {
-        // رشته ساده به جای JSON
-        "expo-channel-name": "production"
-      }
+      package: "fxman.filmonix"
+    },
+    ios: {
+      bundleIdentifier: "fxman.filmonix"
     }
-  };
+  }
 };
